10aedf8b50d104c608ca7489aa6ca618a8398773
Hammad Musafa <hammad.mustafa@itpeoplecorp.com>
2022-08-19 00:08:39 +0530

    updated

--- index copy.js
+++
@@ -1,8 +0,0 @@
-function setTimer() {
-  const javascriptDiv = `<div>
-    <h1> Vanilla Javascript</h1> <input type="text" />
-    <p>${new Date().toLocaleTimeString()} </p>
-  </div>`;
-  document.getElementById("javascriptDiv").innerHTML = javascriptDiv;
-}
-setInterval(setTimer, 1000);
